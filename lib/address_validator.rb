72bbe74eab85d29a398ac251660942b8fabdde4c
David Yun <abookyun@gmail.com>
2015-11-25 18:22:47 +0800

    Regex rules to validate address

--- lib/address_validator.rb
+++ lib/address_validator.rb
@@ -1,6 +1,14 @@
 class AddressValidator < ActiveModel::EachValidator
+  # "(?<zipcode>(^\d{5}|^\d{3})?)(?<city>\D+[縣市])(?<district>\D+?(市區|鎮區|鎮市|[鄉鎮市區]))(?<others>.+)"
   class << self
-    def valid?(zipcode = nil, city = nil, district = nil, address)
+    def valid?(address)
+      /(\A\d{3,5})?\s?(\D+[縣市])(\D+[鄉鎮市區])?(.+)/.match(address)
+    end
+  end
+
+  def validate_each(record, attribute, value)
+    unless self.class.valid?(value)
+      record.errors.add(attribute, options[:message] || :invalid)
     end
   end
 end
